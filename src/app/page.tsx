61925e86277b9c8b2544a4c0983029297cc980fe
lucashsantiago <lucashenriquesantiago@gmail.com>
2025-01-17 11:12:09 -0300

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,31 @@
 //import Link from "next/link";
 
+import { db } from "~/server/db";
+
 const mockUrls = [
-    "https://ytkbwxnd11.ufs.sh/f/Y2jCl0H2Iab8jKGoZz01Gixa2VRwuF0bmcQYgMkeU7hAI8j6"
-]
+  "https://ytkbwxnd11.ufs.sh/f/Y2jCl0H2Iab8jKGoZz01Gixa2VRwuF0bmcQYgMkeU7hAI8j6",
+];
 
 const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
+  id: index + 1,
+  url,
 }));
 
-export default function HomePage() {
-    return (
-        <main className="">
-            <div className="flex flex-wrap"> {
-                mockImages.map((image) => (
-                    <div key={image.id} className="w-1/2 p-4">
-                        <img src={image.url} />
-                    </div>
-                ))
-            }
-            </div>
-        </main>
-    );
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  return (
+    <main className="">
+      <div className="flex flex-wrap">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-1/2 p-4">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
