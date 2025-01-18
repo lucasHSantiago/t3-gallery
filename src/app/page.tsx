a969ec9ef804dee9370328f63a95d72bcbf61854
lucashsantiago <lucashenriquesantiago@gmail.com>
2025-01-18 16:43:11 -0300

    use auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,34 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+async function Images() {
+    const images = await db.query.images.findMany({
+        orderBy: (model, { desc }) => desc(model.id),
+    });
 
-  return (
-    <main className="">
-      <div className="flex flex-wrap">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+    return (
+        <div className="flex flex-wrap">
+            {images.map((image, index) => (
+                <div key={image.id + "-" + index} className="flex w-48 flex-col">
+                    <img src={image.url} />
+                    <div>{image.name}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default async function HomePage() {
+    return (
+        <main className="">
+            <SignedOut>
+                <div className="h-full w-full text-2x1 text-center"> Please sign in above</div>
+            </SignedOut>
+            <SignedIn>
+                <Images />
+            </SignedIn>
+        </main>
+    );
 }
